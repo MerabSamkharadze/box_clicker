d3b77b8d6cf240913f707d817ce4ffca54fdec2a
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-06-10 17:16:51 +0400

    use fetch for get data colors

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 const boxes = document.getElementById("boxes");
 const indicator = document.querySelector(".indicator");
-let colors = [
-  "yellow",
-  "green",
-  "red",
-  "purple",
-  "violet",
-  "indigo",
-  "blue",
-  "orange",
-];
+let colors = [];
 
-for (let color of colors) {
-  let box = document.createElement("div");
-  box.style.backgroundColor = color;
-  box.classList.add("box");
-  boxes.appendChild(box);
-  box.addEventListener("mouseover", function () {
-    indicator.style.backgroundColor = box.style.backgroundColor;
+fetch("https://reqres.in/api/unknown", { method: "GET" })
+  .then((responseInfo) => {
+    if (!responseInfo.ok) {
+      throw responseInfo.status;
+    }
+    return responseInfo.json();
+  })
+  .then((responseData) => {
+    const data = responseData.data;
+    for (let obj of data) {
+      colors.push(obj.color);
+    }
+    console.log(colors);
+
+    for (let color of colors) {
+      let box = document.createElement("div");
+      box.style.backgroundColor = color;
+      box.classList.add("box");
+      boxes.appendChild(box);
+      box.addEventListener("mouseover", function () {
+        indicator.style.backgroundColor = box.style.backgroundColor;
+      });
+    }
   });
-}
